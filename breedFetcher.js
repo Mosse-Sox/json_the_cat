505ab33c58a2333471c5fdabf63ed5d680dcea5d
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-06 23:55:13 +0000

    refactored to inclue index.js and modularize the code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,23 @@
 const request = require("request");
 
-const args = process.argv.slice(2);
-const breed = args.join("%20");
+const fetchBreedDescription = (breedName, callback) => {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, reponse, body) => {
+      if (error) {
+        callback(error, null);
+        process.exit();
+      }
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  (error, reponse, body) => {
-    if (error) {
-      console.log("error: ", error);
-      process.exit;
+      const data = JSON.parse(body);
+
+      if (data.length === 0 || data[0].description === undefined) {
+        callback('No cat was returned, try a new name', null);
+      }
+
+      callback(null, data[0].description);
     }
+  );
+};
 
-    const data = JSON.parse(body);
-    // console.log(data);
-    // console.log(typeof data);
-    if (data.length === 0) {
-      return console.log("No data returned, try another name");
-    }
-
-    
-    console.log(data[0].description);
-  }
-);
+module.exports = fetchBreedDescription;
---
+++ index.js
@@ -0,0 +1,15 @@
+const fetchBreedDescription = require('./breedFetcher');
+
+const args = process.argv.slice(2);
+const breed = args[0];
+
+fetchBreedDescription(breed, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
+
+
+
